b17dee2e81c44e059e58d27c1dd55394e2d9b2ae
Antonio Morales <antmordhar@correo.ugr.es>
2019-12-20 01:39:22 +0100

    Modificados los dockerfiles, Arreglado el coverage Fixes #52

--- APIService/test/API/APIControllerTest.java
+++ APIService/test/API/APIControllerTest.java
@@ -54,42 +54,42 @@ public class APIControllerTest {
         request = new HttpEntity<String>("", headers);
         this.restTemplate.delete("http://"+System.getenv("HOST")+":" + port + "restaurant/pedido/delete/69");
         
-        // assertThat("Borra el pedido por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/pedido/get/69",String.class)
-        // ,is("[]"));
+        assertThat("Borra el pedido por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/pedido/get/69",String.class)
+        ,is("[]"));
 
-        // //Prueba de COCINA------------------------------------------------------------------------------------------------------------------------------
-        // //Prueba de POST && GET------------------------------------------------------------------------------------------------------------------------------
-        // request = 
-        //         new HttpEntity<String>("{\"idmesa\":69,\"nombre\":\"plato0\", \"precio\":1.4, \"cantidad\":1}", headers);
-        // this.restTemplate.postForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/post",request,
-        //     String.class);
-        // assertThat("Inserta cocina por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/get/69",String.class)
-        // ,is(not("[]")));
-        // //Prueba de SEND------------------------------------------------------------------------------------------------------------------------------
-        // request = 
-        // new HttpEntity<String>("", headers);
-        //  assertThat("Sendea cocina Correctamente",this.restTemplate.postForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/send/69",request,
-        //      String.class),is("OK"));
-        // //Prueba de DELETE------------------------------------------------------------------------------------------------------------------------------
-        // request = new HttpEntity<String>("", headers);
-        // this.restTemplate.delete("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/delete/69");
-        // assertThat("Borra la cocina por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/get/69",String.class)
-        // ,is("[]"));
-        // //Prueba de CAMARERO------------------------------------------------------------------------------------------------------------------------------
-        // //Prueba de POST && GET------------------------------------------------------------------------------------------------------------------------------
-        // request = 
-        //         new HttpEntity<String>("{\"idmesa\":69,\"nombre\":\"plato0\", \"precio\":1.4, \"cantidad\":1}", headers);
-        // this.restTemplate.postForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/post",request,
-        //     String.class);
-        // assertThat("Inserta camarero por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/get/69",String.class)
-        // ,is(not("[]")));
-        // //Prueba de SEND------------------------------------------------------------------------------------------------------------------------------         
-        // assertThat(this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/price/69",String.class),is(not("0")));
-        // //Prueba de DELETE------------------------------------------------------------------------------------------------------------------------------
-        // request = new HttpEntity<String>("", headers);
-        // this.restTemplate.delete("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/delete/69");
-        // assertThat("Borra el camarero por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/get/69",String.class)
-        // ,is("[]"));
+        //Prueba de COCINA------------------------------------------------------------------------------------------------------------------------------
+        //Prueba de POST && GET------------------------------------------------------------------------------------------------------------------------------
+        request = 
+                new HttpEntity<String>("{\"idmesa\":69,\"nombre\":\"plato0\", \"precio\":1.4, \"cantidad\":1}", headers);
+        this.restTemplate.postForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/post",request,
+            String.class);
+        assertThat("Inserta cocina por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/get/69",String.class)
+        ,is(not("[]")));
+        //Prueba de SEND------------------------------------------------------------------------------------------------------------------------------
+        request = 
+        new HttpEntity<String>("", headers);
+         assertThat("Sendea cocina Correctamente",this.restTemplate.postForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/send/69",request,
+             String.class),is("OK"));
+        //Prueba de DELETE------------------------------------------------------------------------------------------------------------------------------
+        request = new HttpEntity<String>("", headers);
+        this.restTemplate.delete("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/delete/69");
+        assertThat("Borra la cocina por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/cocina/get/69",String.class)
+        ,is("[]"));
+        //Prueba de CAMARERO------------------------------------------------------------------------------------------------------------------------------
+        //Prueba de POST && GET------------------------------------------------------------------------------------------------------------------------------
+        request = 
+                new HttpEntity<String>("{\"idmesa\":69,\"nombre\":\"plato0\", \"precio\":1.4, \"cantidad\":1}", headers);
+        this.restTemplate.postForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/post",request,
+            String.class);
+        assertThat("Inserta camarero por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/get/69",String.class)
+        ,is(not("[]")));
+        //Prueba de SEND------------------------------------------------------------------------------------------------------------------------------         
+        assertThat(this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/price/69",String.class),is(not("0")));
+        //Prueba de DELETE------------------------------------------------------------------------------------------------------------------------------
+        request = new HttpEntity<String>("", headers);
+        this.restTemplate.delete("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/delete/69");
+        assertThat("Borra el camarero por HTTP",this.restTemplate.getForObject("http://"+System.getenv("HOST")+":" + port + "restaurant/camarero/get/69",String.class)
+        ,is("[]"));
         //Prueba de HELLO------------------------------------------------------------------------------------------------------------------------------
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",String.class)
         ,is("Hello World!"));
